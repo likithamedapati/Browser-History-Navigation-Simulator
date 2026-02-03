a6fcccb4947b3fd20844c834f308dc59693e4fa1
likithamedapati <likithamedapati@gmail.com>
2026-02-03 19:46:13 +0530

    Added browser system setup

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Smart Browser Simulator ")
+print("-" * 40)
+
+homepage = "home.com"
+current_page = homepage
+
+back_stack = []
+forward_stack = []
+history_list = []
+visit_count = {}
+
+print(f"You are on: {current_page}")
